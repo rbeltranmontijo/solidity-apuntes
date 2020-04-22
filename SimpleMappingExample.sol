a242c77610adf73c0a14588053ce8e9d65f73822
Robin Beltran <rbeltranmontijo@gmail.com>
2020-04-21 21:56:31 -0500

    mapping sintaxis

---
+++ SimpleMappingExample.sol
@@ -0,0 +1,18 @@
+pragma solidity ^0.5.13;
+
+contract SimpleMappingExample {
+    
+    // mapping(key => value) nameVariable;
+    mapping(uint => bool) public myMapping;
+    
+    mapping(address => bool) public myAddressMapping;
+    
+    function setValue(uint _index) public {
+        myMapping[_index] = true;
+    }
+    
+    function setMyAddressToTrue() public {
+        myAddressMapping[msg.sender] = true;
+    }
+    
+}
