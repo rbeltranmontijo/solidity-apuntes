3702865c63cb08a50ab3cebb7f5e980c12bc21ba
Robin Beltran <rbeltranmontijo@gmail.com>
2020-04-22 22:24:27 -0500

    mapping y estructuras

---
+++ MappingStructExample.sol
@@ -0,0 +1,42 @@
+pragma solidity ^0.5.13;
+
+contract MappingStructExample {
+    
+    struct Payment {
+        uint amount;
+        uint timestamp;
+    }
+    
+    struct Balance {
+        uint totalBalance;
+        uint numPayments;
+        mapping(uint => Payment) payments;
+    }
+    
+    mapping(address => Balance) public balanceReceived;
+    
+    function getBalance() public view returns(uint) {
+        return address(this).balance;
+    }
+    
+    function sendMoney() public payable {
+        balanceReceived[msg.sender].totalBalance += msg.value;
+        
+        Payment memory payment = Payment(msg.value, now);
+        
+        balanceReceived[msg.sender].payments[balanceReceived[msg.sender].numPayments] = payment;
+        balanceReceived[msg.sender].numPayments++;
+    }
+    
+    function withdrawAllMoney(address payable _to) public {
+        uint balanceToSend = balanceReceived[msg.sender].totalBalance;
+        balanceReceived[msg.sender].totalBalance = 0;
+        _to.transfer(balanceToSend);
+    }
+    
+    function withdrawMoney(address payable _to, uint _amount) public {
+        require(balanceReceived[msg.sender].totalBalance >= _amount, "No tiene suficientes recursos");
+        balanceReceived[msg.sender].totalBalance -= _amount;
+        _to.transfer(_amount);
+    }
+}
